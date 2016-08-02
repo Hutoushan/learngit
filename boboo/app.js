1b49c5286cc7a9e79cc23ecc6508e0aae4ec3d51
Hutoushan <203525877@qq.com>
2016-08-02 10:15:37 +0800

    add boboo

---
+++ boboo/app.js
@@ -0,0 +1,108 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');   //G
+var MongoStore = require('connect-mongo')(session);   //G
+var multer = require('multer');           //G1
+
+var routes = require('./routes/index');
+var settings = require('./settings');     //G
+var flash = require('connect-flash');    //G
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('port',process.env.PORT || 3000);    // G
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(flash());      //G
+
+app.use(session({
+    secret: settings.cookieSecret,
+    key: settings.db,//cookie name
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+    resave: false,
+    saveUninitialized: true,
+    store: new MongoStore({
+        /*db: settings.db,
+         host: settings.host,
+         port: settings.port*/
+        url: 'mongodb://localhost/boboo'
+    })}));
+
+var upload = multer({dest: './public/images'});
+
+app.use(multer({
+    dest: './public/images',
+    rename: function (fieldname, filename) {
+        return filename;
+    }
+}));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));   //G
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+routes(app);    //G
+
+app.listen(app.get('port'),function(){                //G
+  console.log('Express server listening on port'+app.get('port'));    //G
+});
+
+//中间件     G
+/*app.use(session({
+  secrit: settings.cookieSecret,
+  key:settings.db,//cookie name
+  cookie:{maxAge:1000*60*60*24*30},//30 days
+  store:new MongoStore({
+    db:settings.db,
+    host:settings.host,
+    port:settings.port
+  })
+}))*/
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
---
+++ boboo/settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    cookieSecret:'myboboo',
+    db:'boboo',
+    host:'localhost',
+    port:27017
+};
